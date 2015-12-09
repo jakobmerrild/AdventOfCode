0a39f24f3c3599ef831226978f798094398cb6e3
Jakob Merrild <jmer@itu.dk>
2015-12-09 15:54:01 +0100

    Completed day 4

---
+++ AdventOfCode/Day4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string input = @"iwrupvqb";
+            int suffix = 1;
+            while (true)
+            {
+                using (var md5 = MD5.Create())
+                {
+                    var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input + suffix));
+                    //First 5 digits in the hexadecimal representation are 0 when the first 20 bits are 0
+/*                    if (hash[0] == 0 && hash[1] == 0 && (hash[2] & 240) == 0)
+                        break;*/
+                    //First 6 digits in the hexadecimal representation are 0 when the first 24 bits are 0
+                    if (hash[0] == 0 && hash[1] == 0 && hash[2] == 0)
+                        break;
+                }
+                ++suffix;
+            }
+            Console.WriteLine("Answer: {0}", suffix);
+            Console.ReadKey();
+        }
+    }
+}
