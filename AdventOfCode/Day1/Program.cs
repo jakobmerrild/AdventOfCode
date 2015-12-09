4bc5c44ad39d7cdbd3488d18b773f320b4db0f4a
Jakob Merrild <jmer@itu.dk>
2015-12-09 13:04:02 +0100

    Added solution for day 1.

--- AdventOfCode/Day1/Program.cs
+++ AdventOfCode/Day1/Program.cs
@@ -10,6 +10,29 @@ namespace Day1
     {
         static void Main(string[] args)
         {
+            var input = Properties.Resources.Input;
+            var arr = input.ToCharArray();
+            int sum = 0;
+            int basement = -1;
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                switch (arr[i])
+                {
+                    case '(':
+                        sum += 1;
+                        break;
+                    case ')':
+                        sum -= 1;
+                        break;
+                    default:
+                        throw new Exception("Illegal character.");
+                }
+                if (sum < 0 && basement < 0)
+                    basement = i + 1;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(basement);
+            Console.ReadKey();
         }
     }
 }
